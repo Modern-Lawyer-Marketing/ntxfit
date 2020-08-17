a8673c2c41679b05c4604bad90ad6457b119b484
s-fletcher <sfletchertx@gmail.com>
2020-08-17 15:34:14 -0500

    Button styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import '../styles/button.scss';
 
 /**
  * Reusable button component
  */
 const Button = ({ children }) => (
-  <button type="button">{children}</button>
+  <a className="button" type="button">
+    <span>{children}</span>
+  </a>
 );
 
 export default Button;
