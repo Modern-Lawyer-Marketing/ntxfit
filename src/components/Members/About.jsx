0ea5f069e30920efe4e7a1c51aeb85444b772c8b
s-fletcher <sfletchertx@gmail.com>
2020-08-19 18:20:13 -0500

    Added about to who we are page

---
+++ src/components/Members/About.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+import React from 'react';
+import '../../styles/members/about.scss';
+import SectionBox from '../SectionBox';
+
+/**
+ * About section
+ */
+const About = () => (
+  <div className="members-about">
+    <div className="section">
+      <p>Having worked on different aspects of cases over the years, we are committed to supporting diverse interventions to assist parents and children alike. As a group, we focus primarily on highlighting best practice information and growing professional services to better serve the community.</p>
+    </div>
+  </div>
+);
+
+export default About;
--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import MemberGrid from '../components/Members/MemberGrid';
+import About from '../components/Members/About';
 
 /**
  * Landing page
@@ -10,6 +11,7 @@ const Members = () => (
     <Header short backgroundImage={require('../images/members-header.jpg')} backgroundPosition="right">
       <h1>Who We Are</h1>
     </Header>
+    <About />
     <MemberGrid />
   </div>
 );
