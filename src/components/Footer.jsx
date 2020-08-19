b6438726a228e95f1cfd54e54a211e2ce51bf707
s-fletcher <sfletchertx@gmail.com>
2020-08-18 19:57:43 -0500

    Footer mobile friendly

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import '../styles/footer.scss';
+import useWindowDimensions from '../lib/useWindowDimensions';
 
 /**
  * Footer is shown on all pages
  */
-const Footer = () => (
-  <div className="footer">
-    <div className="section">
-      <ul>
-        <li>Home</li>
-        <li>Who We Are</li>
-        <li>Events</li>
-        <li>Resources</li>
-      </ul>
-      <p>© 2020 NTXFIT</p>
-    </div>
-  </div>
-);
+const Footer = () => {
+  const { isMobile } = useWindowDimensions();
 
+  return (
+    <div className="footer">
+      <div className="section">
+        <ul className={isMobile ? 'mobile' : ''}>
+          <li>Home</li>
+          <li>Who We Are</li>
+          <li>Events</li>
+          <li>Resources</li>
+        </ul>
+        <p>© 2020 NTXFIT</p>
+      </div>
+    </div>
+  );
+};
 export default Footer;
