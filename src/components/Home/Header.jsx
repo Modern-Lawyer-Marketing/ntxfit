ee7550d24b389f3772b911d8d74d10aa7be8deb9
s-fletcher <sfletchertx@gmail.com>
2020-08-18 20:05:29 -0500

    Header and section box mobile optimization

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import '../../styles/home/header.scss';
+import useWindowDimensions from '../../lib/useWindowDimensions';
 
 /**
  * Header
  */
-const Header = () => (
-  <div className="home-header">
-    <div className="background-image">
-      <div className="content">
-        <p className="pretitle">North Texas</p>
-        <h1 className="title">Families In Transition</h1>
-        <h2>Professionals Encouraging Healthy Families</h2>
+const Header = () => {
+  const { isMobile } = useWindowDimensions();
+
+  return (
+    <div className={`home-header ${isMobile ? 'mobile' : ''}`}>
+      <div className="background-image">
+        <div className="content">
+          <p>North Texas</p>
+          <h1>Families In Transition</h1>
+          <h2>Professionals Encouraging Healthy Families</h2>
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default Header;
--- src/components/SectionBox.jsx
+++ src/components/SectionBox.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../styles/section-box.scss';
+import useWindowDimensions from '../lib/useWindowDimensions';
 
 /**
  * Renders a reusable section box
  */
-const SectionBox = ({ children, center }) => (
-  <div className="section-box section">
-    <div className={`box ${center && 'center'}`}>{children}</div>
-  </div>
-);
+const SectionBox = ({ children, center }) => {
+  const { isMobile } = useWindowDimensions();
 
+  return (
+    <div className={`section-box section ${isMobile ? 'mobile' : ''}`}>
+      <div className={`box ${center && 'center'}`}>{children}</div>
+    </div>
+  );
+};
 export default SectionBox;
