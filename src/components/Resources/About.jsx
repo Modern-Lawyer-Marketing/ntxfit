ff6acbb1ed24ab566bb87673cc191f8d76615a8f
s-fletcher <sfletchertx@gmail.com>
2020-08-21 11:39:23 -0500

    Optimize image

--- src/components/Resources/About.jsx
+++ src/components/Resources/About.jsx
@@ -8,7 +8,7 @@ import '../../styles/members/about.scss';
 const About = () => (
   <div className="members-about">
     <div className="section">
-      <p>NTXFIT professionals include published authors of books and articles.  Members of NTXFIT also continue to provide trainings with other professionals in support of licensed mental health providers becoming active members of the forensic community.</p>
+      <p>NTXFIT professionals include published authors of books and articles. Members of NTXFIT also continue to provide trainings with other professionals in support of licensed mental health providers becoming active members of the forensic community.</p>
     </div>
   </div>
 );
