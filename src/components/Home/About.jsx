69940894dbc10a19bfdf6089eaa9e67630f34de8
s-fletcher <sfletchertx@gmail.com>
2020-08-20 20:50:53 -0500

    Better optimized home page

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
   return (
     <div className="about">
       <div className={`section ${isMobile ? 'mobile' : ''}`}>
-        <img src={require('../../images/raincoats.jpg')} alt="family" />
+        <div className="thumbnail" style={{ backgroundImage: `url(${require('../../images/raincoats.jpg')})`, backgroundPosition: 'center 75%' }} />
         <div className="content">
           <h2>We Value Families</h2>
           <p>As professionals working with families, we know it is difficult for parents and children during separation and divorce.  Each professional in North Texas Families in Transition (NTXFIT) is an expert in parent functioning and the emotional functioning of parents and children.  Each professional in NTXFIT is an independent practitioner who relies on relevant research and best practices and standards specializing in serving different roles to benefit families that include:</p>
@@ -31,7 +31,7 @@ const About = () => {
       </div>
 
       <div className={`section flip ${isMobile ? 'mobile' : ''}`}>
-        <img src={require('../../images/experience.jpg')} alt="experience" />
+        <div className="thumbnail" style={{ backgroundImage: `url(${require('../../images/handshake-court.jpg')})`, backgroundPosition: '70% 20%' }} />
         <div className="content">
           <h2>Experience Matters</h2>
           <p>Each of the professionals in NTXFIT is highly trained and experienced in working with families involved in litigation.   Individuals in NTXFIT contribute to the knowledge base of other mental health providers and family law attorneys by publishing in peer-reviewed literature, speaking at national conferences, training other providers locally, television news interviews, and serving as a resource for other professionals.  Each professional in NTXFIT demonstrates integrity, compassion, and leadership in the DFW community and to the profession of mental health providers.</p>
@@ -39,7 +39,7 @@ const About = () => {
       </div>
 
       <div className={`section ${isMobile ? 'mobile' : ''}`}>
-        <img src={require('../../images/pen-and-paper.jpg')} alt="pen and paper" />
+        <div className="thumbnail" style={{ backgroundImage: `url(${require('../../images/pen-and-paper.jpg')})`, backgroundPosition: '70%' }} />
         <div className="content">
           <h2>Why We Created NTXFIT</h2>
           <p>A limited number of professionals gathered to serve as resources to each other and to the professional community to contribute to the expertise in the field of family law litigation as mental health professionals.   As leaders teaching others how to do forensic work, NTXFIT continues to offer twice a year advanced trainings to encourage other mental health professionals to accept cases involving litigation.   We also encourage other professionals to form their own organizations for similar purpose as has been done by a group of professionals who formed THETAFF.</p>
