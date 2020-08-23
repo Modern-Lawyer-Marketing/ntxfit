eb86c58465529297bac3992cbc9672f9aefddf42
s-fletcher <sfletchertx@gmail.com>
2020-08-23 15:52:02 -0500

    Update member

--- src/components/Members/MemberCard.jsx
+++ src/components/Members/MemberCard.jsx
@@ -5,22 +5,24 @@ import '../../styles/members/member-card.scss';
  * About section
  */
 const MemberCard = ({ name, image, license, email, website, phone, services }) => (
-  // <div>
   <div className="member-card">
     <div className="image" style={{ backgroundImage: `url(${image})` }} />
     <div className="content">
-      <h3>{name}</h3>
-      <p>{license}</p>
-      <p><a href={`https://${website}`}>{website}</a></p>
-      <p><a href={`mailto:${email}(${name})`}>{email}</a></p>
-      <p>Phone: <a href={`tel:${phone.replace(/[-\s]/g, '')}`}>{phone}</a></p>
-      <p>Services:</p>
-      <ul className="services">
-        {services.map((val, i) => (<li key={val + i}>{val}</li>))}
-      </ul>
+      {name && <h3>{name}</h3>}
+      {license && <p>{license}</p>}
+      {website && <p><a target="_blank" rel="noopener noreferrer" href={`http://${website}`}>{website}</a></p>}
+      {email && <p><a href={`mailto:${email}(${name})`}>{email}</a></p>}
+      {phone && <p>Phone: <a href={`tel:${phone.replace(/[-\s]/g, '')}`}>{phone}</a></p>}
+      {services && (
+      <>
+        <p>Services:</p>
+        <ul className="services">
+          {services.map((val, i) => (<li key={val + i}>{val}</li>))}
+        </ul>
+      </>
+      )}
     </div>
   </div>
-  // </div>
 );
 
 export default MemberCard;
