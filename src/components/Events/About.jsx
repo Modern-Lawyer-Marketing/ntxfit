3f458c06258d1e3e097dcd0c35997897a49489c7
s-fletcher <sfletchertx@gmail.com>
2020-09-08 18:17:28 -0500

    Change event subtitle

--- src/components/Events/About.jsx
+++ src/components/Events/About.jsx
@@ -10,7 +10,7 @@ const About = () => (
       <h2>Professional Trainings Available</h2>
       <div>
         {/* eslint-disable-next-line max-len */}
-        <p>North Texas Families in Transition provides training for professionals to encourage more trained professionals to assist families involved in litigation.</p>
+        <p>North Texas Families in Transition provides training to assist professionals working with families involved in litigation in staying up to date with best practices and current research.</p>
       </div>
       <a href="https://northtexasfit.us3.list-manage.com/subscribe/post?u=e6981a181982f79c076ceb3b3&id=770b503343" target="_blank" rel="noopener noreferrer" className="get-notified-button button">Get Notified of Upcoming Events</a>
     </div>
