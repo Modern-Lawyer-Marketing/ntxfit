c7bd6c2f37468394f45a64391bc493f3e0ba6654
Sam Fletcher <sfletchertx@gmail.com>
2023-02-05 14:06:22 -0600

    Fix guideline links

--- src/pages/Resources/Professionals.jsx
+++ src/pages/Resources/Professionals.jsx
@@ -14,11 +14,11 @@ const Professionals = () => (
     <div className="resources-content content-item">
       <SectionBox>
         <ul>
-          <li><a href="http://www.afccnet.org/Portals/0/PublicDocuments/Guidelines/AFCCGuidelinesforParentingcoordinationnew.pdf" target="_blank" rel="noopener noreferrer">Parenting Coordination/Facilitation</a></li>
-          <li><a href="http://www.afccnet.org/Portals/0/PublicDocuments/CEFCP/Guidelines%20for%20Court%20Involved%20Therapy%20AFCC.pdf" target="_blank" rel="noopener noreferrer">Court Involved Therapy</a></li>
-          <li><a href="http://www.afccnet.org/Portals/0/PublicDocuments/Guidelines/ModelStdsChildCustodyEvalSept2006.pdf" target="_blank" rel="noopener noreferrer">Child Custody Evaluation</a></li>
-          <li><a href="http://www.afccnet.org/Portals/0/PublicDocuments/CEFCP/ModelStandardsOfPracticeForFamilyAndDivorceMediation.pdf" target="_blank" rel="noopener noreferrer">Mediation</a></li>
-          <li><a href="http://www.afccnet.org/Portals/0/PublicDocuments/Guidelines/Supervised_Visitation_Nework-StandardsFinal7-14-06.pdf" target="_blank" rel="noopener noreferrer">Supervised Visitation</a></li>
+          <li><a href="/documents/AFCC_Guidelines_for_Parenting_Coordination_2019.pdf" target="_blank" rel="noopener noreferrer">Parenting Coordination/Facilitation</a></li>
+          <li><a href="/documents/AFCC_Guidelines_for_Court_Involved_Therapy.pdf" target="_blank" rel="noopener noreferrer">Court Involved Therapy</a></li>
+          <li><a href="/documents/AFCC_Guidelines_for_Parenting_Plan_Evaluations_in_Family_Law_Cases.pdf" target="_blank" rel="noopener noreferrer">Child Custody Evaluation</a></li>
+          <li><a href="/documents/AFCC_Model_Standards_of_Practice_for_Family_and_Divorce_Mediation.pdf" target="_blank" rel="noopener noreferrer">Mediation</a></li>
+          {/* <li><a href="http://www.afccnet.org/Portals/0/PublicDocuments/Guidelines/Supervised_Visitation_Nework-StandardsFinal7-14-06.pdf" target="_blank" rel="noopener noreferrer">Supervised Visitation</a></li> */}
         </ul>
       </SectionBox>
     </div>
