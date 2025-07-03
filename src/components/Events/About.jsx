fd9c353b2dff885a3ed8f676e44bf753f7a97639
Sam Fletcher <sfletchertx@gmail.com>
2025-07-03 10:32:14 -0500

    Update mailing list link

--- src/components/Events/About.jsx
+++ src/components/Events/About.jsx
@@ -12,7 +12,7 @@ const About = () => (
         {/* eslint-disable-next-line max-len */}
         <p>North Texas Families in Transition provides training to assist professionals working with families involved in litigation in staying up to date with best practices and current research.</p>
       </div>
-      <a href="https://northtexasfit.us3.list-manage.com/subscribe/post?u=e6981a181982f79c076ceb3b3&id=770b503343" target="_blank" rel="noopener noreferrer" className="get-notified-button button">Get Notified of Upcoming Events</a>
+      <a href="http://eepurl.com/hQXwxP" target="_blank" rel="noopener noreferrer" className="get-notified-button button">Get Notified of Upcoming Events</a>
     </div>
   </div>
 );
